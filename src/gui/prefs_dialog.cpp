598e6cbda4ece384d63cb79a0997fe9b5a3c5591
Jesse Chappell <jesse@essej.net>
2005-01-27 18:49:28 +0000

    missing files

---
+++ src/gui/prefs_dialog.cpp
@@ -0,0 +1,122 @@
+/*
+** Copyright (C) 2004 Jesse Chappell <jesse@essej.net>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**  
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**  
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**  
+*/
+
+#include <wx/wx.h>
+#include <wx/listctrl.h>
+#include <wx/spinctrl.h>
+#include <wx/notebook.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "gui_frame.hpp"
+#include "prefs_dialog.hpp"
+#include "config_panel.hpp"
+#include "keys_panel.hpp"
+#include "midi_bind_panel.hpp"
+
+#include "loop_control.hpp"
+#include "keyboard_target.hpp"
+
+using namespace SooperLooperGui;
+using namespace std;
+
+#define DEFAULT_OSC_PORT 9951
+
+
+enum {
+	ID_ConnectButton = 8000,
+	ID_CloseButton,
+	ID_DisconnectButton,
+	ID_MidiBrowseButton,
+	ID_DefaultButton,
+	ID_CommitButton,
+	ID_RevertButton
+};
+
+BEGIN_EVENT_TABLE(SooperLooperGui::PrefsDialog, wxFrame)
+	EVT_CLOSE(SooperLooperGui::PrefsDialog::on_close)
+
+END_EVENT_TABLE()
+
+	
+// ctor(s)
+PrefsDialog::PrefsDialog(GuiFrame * parent, wxWindowID id, const wxString& title,
+		       const wxPoint& pos,
+		       const wxSize& size,
+		       long style ,
+		       const wxString& name)
+
+	: wxFrame ((wxWindow *)parent, id, title, pos, size, style, name), _parent (parent)
+{
+	init();
+}
+
+PrefsDialog::~PrefsDialog()
+{
+
+}
+
+
+void PrefsDialog::init()
+{
+	wxBoxSizer * topsizer = new wxBoxSizer(wxVERTICAL);
+	
+	_notebook = new wxNotebook (this, -1);
+	wxNotebookSizer *nbs = new wxNotebookSizer( _notebook );
+	
+	_config_panel = new ConfigPanel (_parent, _notebook, -1);
+	_notebook->AddPage (_config_panel, wxT("Connection"), true);
+	
+	_keys_panel = new KeysPanel(_parent, _notebook, -1);
+	_notebook->AddPage (_keys_panel, wxT("Key Bindings"), false);
+
+	_midi_panel = new MidiBindPanel(_parent, _notebook, -1);
+	_notebook->AddPage (_midi_panel, wxT("MIDI Bindings"), false);
+
+	topsizer->Add (nbs, 1, wxEXPAND|wxALL, 6);
+	
+	
+	this->SetAutoLayout( true );     // tell dialog to use sizer
+	this->SetSizer( topsizer );      // actually set the sizer
+	topsizer->Fit( this );            // set size to minimum size as calculated by the sizer
+	topsizer->SetSizeHints( this );   // set size hints to honour mininum size
+}
+
+void PrefsDialog::refresh_state ()
+{
+	_config_panel->refresh_state();
+	_keys_panel->refresh_state();
+	_midi_panel->refresh_state();
+}
+
+
+void PrefsDialog::on_close (wxCloseEvent &ev)
+{
+	if (!ev.CanVeto()) {
+		
+		Destroy();
+	}
+	else {
+		ev.Veto();
+		
+		Show(false);
+	}
+}
---
+++ src/gui/prefs_dialog.hpp
@@ -0,0 +1,82 @@
+/*
+** Copyright (C) 2004 Jesse Chappell <jesse@essej.net>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**  
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**  
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**  
+*/
+
+#ifndef __sooperlooper_prefs_dialog__
+#define __sooperlooper_prefs_dialog__
+
+
+#include <wx/wx.h>
+#include <wx/listbase.h>
+
+#include <string>
+#include <vector>
+#include <sigc++/object.h>
+
+class wxListCtrl;
+class wxSpinCtrl;
+class wxNotebook;
+
+namespace SooperLooperGui {
+
+class GuiFrame;
+class KeyboardTarget;
+class ConfigPanel;
+class KeysPanel;
+class MidiBindPanel;
+	
+class PrefsDialog
+	: public wxFrame,  public SigC::Object
+{
+  public:
+	
+	// ctor(s)
+	PrefsDialog(GuiFrame * parent, wxWindowID id, const wxString& title,
+		   const wxPoint& pos = wxDefaultPosition,
+		   const wxSize& size = wxSize(400,600),
+		   long style = wxDEFAULT_FRAME_STYLE,
+		   const wxString& name = wxT("PrefsDialog"));
+
+	virtual ~PrefsDialog();
+
+	void refresh_state();
+
+	
+   protected:
+
+	void init();
+
+	void on_close (wxCloseEvent &ev);
+
+	wxNotebook * _notebook;
+	
+	ConfigPanel * _config_panel;
+	KeysPanel    * _keys_panel;
+	MidiBindPanel * _midi_panel;
+	
+	GuiFrame * _parent;
+	
+  private:
+    // any class wishing to process wxWindows events must use this macro
+    DECLARE_EVENT_TABLE()
+	
+};
+
+};
+
+#endif
