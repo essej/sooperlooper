3a694c387619e295ae29cb8b991f7465b1a7f978
Jesse Chappell <jesse@essej.net>
2008-09-26 01:04:26 +0000

    updated version

--- mac/makedmg.sh
+++ mac/makedmg.sh
@@ -1,7 +1,7 @@
 version=`grep const ../version.h | cut -d" " -f7 | sed -e 's/[";]//g'`     
-echo Version is $version
+echo Version is ${version}$1
 
 ./lipo.sh
 
-rm -f SooperLooper-${version}.dmg
-hdiutil create -fs HFS+ -volname SooperLooper-${version} -srcfolder macdist SooperLooper-${version}.dmg
+rm -f SooperLooper-${version}$1.dmg
+hdiutil create -fs HFS+ -volname SooperLooper-${version}$1 -srcfolder macdist SooperLooper-${version}$1.dmg
