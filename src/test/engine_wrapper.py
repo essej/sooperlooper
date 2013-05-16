b18d0f134adde3d514cbc26b8aa75cffaf33c1f8
Kaspar Bumke <kaspar.bumke@gmail.com>
2013-05-16 14:26:49 +0100

    adding overdub, looking at nose

--- src/test/engine_wrapper.py
+++ src/test/engine_wrapper.py
@@ -36,22 +36,47 @@ class Engine (test_engine.TestEngine):
                 ,"PAUSE_ON"
                 ,"PAUSE_OFF"
                 )
+                #,"REDO_TOG" XXX what effect do they have exactly?
+                #,"QUANT_TOG"
+                #,"ROUND_TOG"
 
         self.states = TwoWayDict() 
         for member in test_engine.__dict__.iteritems():
             if "LooperState" in member[0]:
                 self.states[member[1]] = member[0][11:]
 
-        self.auto_states = [ self.states["Unknown"]
-                           , self.states["WaitStart"] 
-                           , self.states["WaitStop"] 
-                           , self.states["TriggerPlay"]
-                           , self.states["OneShot"]
-                           , self.states["Undo"]
-                           , self.states["UndoAll"]
-                           , self.states["Redo"]
-                           , self.states["RedoAll"]
-                           ]
+        self.auto_states = [  self.states["Unknown"]
+                            , self.states["WaitStart"] 
+                            , self.states["WaitStop"] 
+                            , self.states["TriggerPlay"]
+                            , self.states["OneShot"]
+                            , self.states["Undo"]
+                            , self.states["UndoAll"]
+                            , self.states["Redo"]
+                            , self.states["RedoAll"]
+                                                         ]
+
+        self.typical =   {
+                 self.commands.index("REPLACE"   ): self.states["Replacing"]
+                ,self.commands.index("REVERSE"   ): self.states["Playing"]
+                ,self.commands.index("SCRATCH"   ): self.states["Scratching"]
+                ,self.commands.index("RECORD"    ): self.states["Recording"]
+                ,self.commands.index("OVERDUB"   ): self.states["Overdubbing"]
+                ,self.commands.index("MULTIPLY"  ): self.states["Multiplying"]
+                ,self.commands.index("INSERT"    ): self.states["Inserting"]
+                ,self.commands.index("MUTE"      ): self.states["Muted"]
+                ,self.commands.index("DELAY"     ): self.states["Delay"]
+                ,self.commands.index("ONESHOT"   ): self.states["Playing"]#XXX or Off
+                ,self.commands.index("TRIGGER"   ): self.states["Playing"]
+                ,self.commands.index("SUBSTITUTE"): self.states["Substitute"]
+                ,self.commands.index("UNDO_ALL"  ): self.states["Off"]
+                ,self.commands.index("REDO_ALL"  ): self.states["Playing"]
+                ,self.commands.index("MUTE_ON"   ): self.states["Muted"]
+                ,self.commands.index("MUTE_OFF"  ): self.states["Playing"]
+                ,self.commands.index("PAUSE"     ): self.states["Paused"]
+                ,self.commands.index("PAUSE_ON"  ): self.states["Paused"]
+                ,self.commands.index("PAUSE_OFF" ): self.states["Playing"]
+                                                                               }
 
         #self.commands_per_state = {}
         #for key in self.states:
--- src/test/testdef_simple_state.py
+++ src/test/testdef_simple_state.py
@@ -5,6 +5,7 @@ import unittest
 import engine_wrapper
 
 class simpleStateTest(unittest.TestCase):
+
     def setUp(self):
         self.engine = engine_wrapper.Engine("SL_test")
         self.maxDiff = None
@@ -13,8 +14,8 @@ class simpleStateTest(unittest.TestCase):
         del self.engine
 
     def assertState(self, state, msg=None):
-            actual_state = self.engine.getState()
-            self.assertEqual(actual_state, self.engine.states[state], self.engine.states[actual_state] + " != " +  state)
+       actual_state = self.engine.getState()
+       self.assertEqual(actual_state, self.engine.states[state], self.engine.states[actual_state] + " != " +  state if msg is None else msg)
 
     def _testAll(self, should_be_state_string, ignore=None):
         should_be_state = self.engine.states[should_be_state_string]
@@ -30,8 +31,15 @@ class simpleStateTest(unittest.TestCase):
                 actual[c] = self.engine.states[self.engine.getState()]
         self.assertDictEqual(should_be, actual)
 
+    def _testAllTypical(self, ignore=None):
+        if ignore is None:
+            ignore = []
+        should_be = {}
+        actual = {}
+
     def testRecord(self):
-        print "record"
+        #RECORD should always results in a recording state no matter the current state
         self.engine.request("RECORD")
         time.sleep(0.001)
         self.assertState("Recording")
+
