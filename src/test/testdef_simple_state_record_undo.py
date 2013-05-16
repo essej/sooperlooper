1ce5619d176e467fba456d10f00ab509c9d54189
Kaspar Bumke <kaspar.bumke@gmail.com>
2013-05-16 02:33:28 +0100

    removing SWIG generated .py, adding record_undo

---
+++ src/test/testdef_simple_state_record_undo.py
@@ -0,0 +1,55 @@
+import time
+import testdef_simple_state
+
+class simpleStateTestsRecordUndo(testdef_simple_state.simpleStateTest):
+
+    def setUp(self):
+        testdef_simple_state.simpleStateTest.setUp(self)
+        self.engine.request("RECORD")
+        self.engine.request("RECORD")
+        self.engine.request("UNDO")
+
+    def testOff(self):
+        time.sleep(0.001)
+        self.assertState("Off")
+
+    def testRecord(self):
+        self.engine.request("RECORD")
+        time.sleep(0.001)
+        self.assertState("Recording")
+
+    def testUndo(self):
+        self.engine.request("RECORD")
+        self.engine.request("UNDO")
+        time.sleep(0.001)
+        self.assertState("Off")
+
+    def testUndoAll(self):
+        self.engine.request("RECORD")
+        self.engine.request("UNDO_ALL")
+        time.sleep(0.001)
+        self.assertState("Off")
+
+    def testMuteOn(self):
+        self.engine.request("MUTE_ON")
+        time.sleep(0.001)
+        self.assertState("OffMuted")
+
+    def testDelay(self):
+        self.engine.request("DELAY")
+        time.sleep(0.001)
+        self.assertState("Delay")
+
+    def testRedo(self):
+        self.engine.request("REDO")
+        time.sleep(0.001)
+        self.assertState("Playing")
+
+    def testRedoAll(self):
+        self.engine.request("REDO_ALL")
+        time.sleep(0.001)
+        self.assertState("Playing")
+
+    def testAllOff(self):
+        self._testAll("Off", ignore=[ "RECORD","DELAY","MUTE_ON","MUTE", "REDO", "REDO_ALL"])
+
