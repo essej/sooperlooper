d0274cdde58ec6b180055a5ac915557b01c6cbfc
Jesse Chappell <jesse@essej.net>
2014-02-06 16:41:36 -0500

    fixes to support compiling on mavericks, and to support a rubberband built against accelerate

--- libs/pbd/pbd/stl_delete.h
+++ libs/pbd/pbd/stl_delete.h
@@ -24,7 +24,7 @@
 /* To actually use any of these deletion functions, you need to
    first include the revelant container type header.
 */
-#if defined(_CPP_VECTOR) || defined(_GLIBCXX_VECTOR) || defined(__SGI_STL_VECTOR)
+#if defined(_CPP_VECTOR) || defined(_GLIBCXX_VECTOR) || defined(__SGI_STL_VECTOR) || defined(_LIBCPP_VECTOR)
 template<class T> void vector_delete (std::vector<T *> *vec) 
 {
 	typename std::vector<T *>::iterator i;
