87bddc5ef65c90b270a3a7c3d41528568dfbad51
Jesse Chappell <jesse@sonosaurus.com>
2020-06-11 11:48:57 -0400

    commit missing mac build related files

--- mac/makedmg64.sh
+++ mac/makedmg64.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
+
 version=`grep const ../version.h | cut -d" " -f7 | sed -e 's/[";]//g'`     
 echo Version is ${version}$1
 
 #./lipo.sh
 
-./codesign.sh
+if [ -f ./codesign.sh ] ; then
+  ./codesign.sh
+fi
 
 rm -f SooperLooper-64bit-${version}$1.dmg
 hdiutil create -fs HFS+ -volname SooperLooper-64bit-${version}$1 -srcfolder macdist SooperLooper-64bit-${version}$1.dmg
