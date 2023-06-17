d358a33337a71fc9a31041c6dc4ea94a9760b396
essej <jesse@essej.net>
2023-06-17 11:45:18 -0400

    makedmg64.sh update

--- mac/makedmg64.sh
+++ mac/makedmg64.sh
@@ -5,11 +5,15 @@ echo Version is ${version}$1
 
 #./lipo.sh
 
+
 # can't notarize it yet because the embedded sooperlooper binary links to libjack so we can't use hardened runtime
 
 if [ -f ./codesign.sh ] ; then
   ./codesign.sh
  # (cd macdist; ../notarize-app.sh SooperLooper.app )
+
+  (cd macdist; ../notarize-app.sh SooperLooperAU64.component )
+
 fi
 
 DMGNAME=SooperLooper-64bit-${version}$1.dmg
